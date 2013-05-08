e31a0e3686756291356bdd3c5340d25d4d0f8348
jamesl1001 <jamesl1001@msn.com>
2013-05-08 23:41:40 +0100

    Initial markup and styling

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>mysql-voting-system</title>
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+	<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+</head>
+<body>
+	<h1>mysql-voting-system</h1>
+	<p>A PHP voting system using MySQL.</p>
+
+	<hr>
+
+	<div class="item" id="item-1">
+		<span class="item-title">Item 1</span>
+
+		<div class="vote-up">vote up</div>
+		<div class="vote-down">vote down</div>
+
+		<div class="vote-bar">
+			<div class="vote-bar-up"></div>
+			<div class="vote-bar-down"></div>
+		</div>
+	</div>
+
+
+	<div class="item" id="item-2">
+		<span class="item-title">Item 2</span>
+
+		<div class="vote-up">vote up</div>
+		<div class="vote-down">vote down</div>
+
+		<div class="vote-bar">
+			<div class="vote-bar-up"></div>
+			<div class="vote-bar-down"></div>
+		</div>
+	</div>
+
+
+	<div class="item" id="item-3">
+		<span class="item-title">Item 3</span>
+
+		<div class="vote-up">vote up</div>
+		<div class="vote-down">vote down</div>
+
+		<div class="vote-bar">
+			<div class="vote-bar-up"></div>
+			<div class="vote-bar-down"></div>
+		</div>
+	</div>
+</body>
+</html>
